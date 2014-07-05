d208db999d360d4e21812163e88b68aecd610b5f
FRKodes <frkalderon@gmail.com>
2014-07-05 15:56:18 -0500

    Fixing some things
    Raiding the debug flag to true to can access to the error managing
    window.
    Making able to manage timestamps at User model.

--- app/models/User.php
+++ app/models/User.php
@@ -7,8 +7,6 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	public $timestamps = false;
-
 	protected $fillable = ['username', 'email', 'password'];
 	
 	// protected $guarded = "";/*opposite to fillable*/
